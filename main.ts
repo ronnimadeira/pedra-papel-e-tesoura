d2c2bc6c3c552d8cfe016cba70a779744b80d4d7
ronnimadeira <115476262+ronnimadeira@users.noreply.github.com>
2023-08-28 14:21:23 -0300

    Atualizar 'pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
-let tool = 0
 input.onGesture(Gesture.Shake, function () {
-    tool = randint(0, 2)
-    if (tool == 0) {
+    escolha = randint(0, 2)
+    if (escolha == 0) {
         basic.showIcon(IconNames.SmallSquare)
-    } else if (tool == 1) {
+    } else if (escolha == 1) {
         basic.showIcon(IconNames.Square)
     } else {
         basic.showIcon(IconNames.Scissors)
     }
+    music.play(music.tonePlayable(370, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    basic.pause(200)
 })
+let escolha = 0
+basic.showIcon(IconNames.Happy)
